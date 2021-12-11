e7320ad041d15a362130651098fee85b07172908
Nass <nassim.chouirfa@gmail.comgit config user.namegit config --global user.email nassim.chouirfa@gmail.com>
2021-12-11 18:26:55 +0100

    architecture + création de class terminé C.N

---
+++ public/module/class.js
@@ -0,0 +1,22 @@
+class Boss {
+    constructor(name, hp, pa) {
+        this.name = name;
+        this.hp = hp;
+        this.pa = pa;
+    }
+}
+
+class Hero{
+    constructor(name, hp, pa){
+        this.name = name;
+        this.hp = hp;
+        this.pa = pa;
+        this.etat = "alive";
+        this.defense = () => {
+
+        }
+        this.attack = () =>{
+            
+        }
+    }
+}
